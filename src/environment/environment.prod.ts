d1306fc3fded8f2c1cfd3f32576cddf028489005
Uday Goel <71921608+Deadalus1901@users.noreply.github.com>
2025-02-09 16:56:29 +0530

    Update environment.prod.ts

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://banking-portal-api-udayg1901-dev.apps.rm3.7wse.p1.openshiftapps.com/api',
+  apiUrl: 'http://banking-portal-api-udayg1901-dev.apps.rm3.7wse.p1.openshiftapps.com/api/api',
   tokenName :  "authToken",
   origin:'https://banking-portal-api-udayg1901-dev.apps.rm3.7wse.p1.openshiftapps.com' 
 };
